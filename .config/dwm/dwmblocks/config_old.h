2df6c7d47aba1d41323c4cfcf80b3d813bb33990
Jonas Ornfelt <vmornf@gmail.com>
2022-05-17 20:11:32 +0200

    Dwmblocks scripts update

--- .config/dwm/dwmblocks/config_old.h
+++ .config/dwm/dwmblocks/config_old.h
@@ -31,14 +31,15 @@
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 	{"",	"~/.local/bin/my_scripts/spotify_dwmblocks.sh",	5,	12},
-	{"",	"~/.local/bin/statusbar/sb-internet",	5,	6},
 	/* {"",	"~/.local/bin/statusbar/sb-forecast",	18000,	20}, */
 	/* {"",	"~/.local/bin/statusbar/sb-volumetwo.sh",	0,	11}, */
 	{"   ",  "~/.local/bin/dwmblocks/weather", 	 1800, 				 5},
 	{"  ",  "~/.local/bin/dwmblocks/cputemp", 	5, 					4},
+	/* {"",	"~/.local/bin/statusbar/sb-internet",	5,	4}, */
 	{" ",	"~/.local/bin/statusbar/sb-volume",	0,	10},
 	{" ",	"~/.local/bin/statusbar/sb-battery",	5,	3},
 	{"  ",	"~/.local/bin/statusbar/sb-clock",	60,	1},
+	/* {"🕒 ",	"~/.local/bin/statusbar/sb-clock",	60,	1}, */
 };
 
 /*Icons:       婢墳  */
