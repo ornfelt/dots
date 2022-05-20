7accdaf412e9e372dc3106c29ce893f3bdc6727b
Jonas Ornfelt <vmornf@gmail.com>
2022-05-20 09:59:09 +0200

    Dwmblocks scripts update

--- bin/my_scripts/script_copy.sh
+++ bin/my_scripts/script_copy.sh
@@ -13,45 +13,92 @@ secondOpt="${textSplit[1]}"
 thirdOpt="${textSplit[2]}"
 fourthOpt="${textSplit[3]}"
 fifthOpt="${textSplit[4]}"
-sixthOpt="${textSplit[4]}"
-seventhOpt="${textSplit[4]}"
-eightOpt="${textSplit[4]}"
-ninthOpt="${textSplit[4]}"
-tenthOpt="${textSplit[4]}"
-eleventhOpt="${textSplit[4]}"
-twelfthOpt="${textSplit[4]}"
-thirteenthOpt="${textSplit[4]}"
-fourteenthOpt="${textSplit[4]}"
-fifteenthOpt="${textSplit[4]}"
-sixteenthOpt="${textSplit[4]}"
-seventeenthOpt="${textSplit[4]}"
-eighteenthOpt="${textSplit[4]}"
-nineteenthOpt="${textSplit[4]}"
-twentiethOpt="${textSplit[4]}"
-twentyfirstOpt="${textSplit[4]}"
+sixthOpt="${textSplit[5]}"
+seventhOpt="${textSplit[6]}"
+eighthOpt="${textSplit[7]}"
+ninthOpt="${textSplit[8]}"
+tenthOpt="${textSplit[9]}"
+eleventhOpt="${textSplit[10]}"
+twelfthOpt="${textSplit[11]}"
+thirteenthOpt="${textSplit[12]}"
+fourteenthOpt="${textSplit[13]}"
+fifteenthOpt="${textSplit[14]}"
+sixteenthOpt="${textSplit[15]}"
+seventeenthOpt="${textSplit[16]}"
+eighteenthOpt="${textSplit[17]}"
+nineteenthOpt="${textSplit[18]}"
+twentiethOpt="${textSplit[19]}"
+twentyfirstOpt="${textSplit[20]}"
 #otherOpt=$(echo $fileText | sed -n '1p')
 #otherOpt=$(echo $fileText | awk 'NR==1')
 
 # Variable passed to rofi
-options="$firstOpt\n$secondOpt\n$thirdOpt\n$fourthOpt\n$fifthOpt\n$sixthOpt\n$seventhOpt\n$eightOpt\n$ninthOpt\n$tenthOpt\n$eleventhOpt\n$twelfthOpt\n$thirteenthOpt\n$fourteenthOpt\n$fifteenthOpt\n$sixteenthOpt\n$seventhOpt\n$eighteenthOpt\n$nineteenthOpt\n$twentiethOpt\n$twentyfirstOpt"
+options="$firstOpt\n$secondOpt\n$thirdOpt\n$fourthOpt\n$fifthOpt\n$sixthOpt\n$seventhOpt\n$eighthOpt\n$ninthOpt\n$tenthOpt\n$eleventhOpt\n$twelfthOpt\n$thirteenthOpt\n$fourteenthOpt\n$fifteenthOpt\n$sixteenthOpt\n$seventhOpt\n$eighteenthOpt\n$nineteenthOpt\n$twentiethOpt\n$twentyfirstOpt"
 
-chosen="$(echo -e "$options" | rofi -theme "/home/jonas/.config/rofi/themes/gruvbox/gruvbox-dark2.rasi" -p "Choose a command" -dmenu -selected-row 0)"
+chosen="$(echo -e "$options" | rofi -theme "/home/jonas/.config/rofi/themes/gruvbox/gruvbox-dark2.rasi" -p "Choose a command to copy" -dmenu -selected-row 0)"
 
 case $chosen in
     $firstOpt)
-		#urxvt -e bash -c 'nvim ~/.local/bin/my_scripts/script_help_docs/awk.txt; zsh'
-		echo "1" | xclip -sel clip
+		echo "$firstOpt" | xclip -sel clip
         ;;
     $secondOpt)
-		echo "2" | xclip -sel clip
+		echo "$secondOpt" | xclip -sel clip
         ;;
     $thirdOpt)
-		echo "3" | xclip -sel clip
+		echo "$thirdOpt" | xclip -sel clip
         ;;
     $fourthOpt)
-		echo "4" | xclip -sel clip
+		echo "$fourthOpt" | xclip -sel clip
         ;;
     $fifthOpt)
-		echo "5" | xclip -sel clip
+		echo "$fifthOpt" | xclip -sel clip
+        ;;
+    $sixthOpt)
+		echo "$sixthOpt" | xclip -sel clip
+        ;;
+    $seventhOpt)
+		echo "$seventhOpt" | xclip -sel clip
+        ;;
+    $eighthOpt)
+		echo "$eighthOpt" | xclip -sel clip
+        ;;
+    $ninthOpt)
+		echo "$ninthOpt" | xclip -sel clip
+        ;;
+    $tenthOpt)
+		echo "$tenthOpt" | xclip -sel clip
+        ;;
+    $eleventhOpt)
+		echo "$eleventhOpt" | xclip -sel clip
+        ;;
+    $twelfthOpt)
+		echo "$twelfthOpt" | xclip -sel clip
+        ;;
+    $thirteenthOpt)
+		echo "$thirteenthOpt" | xclip -sel clip
+        ;;
+    $fourteenthOpt)
+		echo "$fourteenthOpt" | xclip -sel clip
+        ;;
+    $fifteenthOpt)
+		echo "$fifteenthOpt" | xclip -sel clip
+        ;;
+    $sixteenthOpt)
+		echo "$sixteenthOpt" | xclip -sel clip
+        ;;
+    $seventeenthOpt)
+		echo "$seventeenthOpt" | xclip -sel clip
+        ;;
+    $eighteenthOpt)
+		echo "$eighteenthOpt" | xclip -sel clip
+        ;;
+    $nineteenthOpt)
+		echo "$nineteenthOpt" | xclip -sel clip
+        ;;
+    $twentiethOpt)
+		echo "$twentiethOpt" | xclip -sel clip
+        ;;
+    $twentyfirstOpt)
+		echo "$twentyfirstOpt" | xclip -sel clip
         ;;
 esac
