5c2196680c387d03468a70cf4b853aff8c392a5e
Jonas <jonasornfelt@outlook.com>
2022-06-11 09:53:17 +0200

    Minor changes

--- .config/st/config.h
+++ .config/st/config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "JetBrainsMono Nerd Font:size=11:style=bold:antialias=true:autohint=true";
+static char *font = "JetBrainsMono Nerd Font:size=11:style=regular:antialias=true:autohint=true";
 /* static char *font = "mono:pixelsize=12:antialias=true:autohint=true"; */
 static char *font2[] = { "JoyPixels:pixelsize=10:antialias=true:autohint=true" };
 static int borderpx = 2;
