bc3fe5c4194e0403bed6765c34ae6c720b3952b2
ornfelt <jonasornfelt@outlook.com>
2024-06-20 06:25:45 +0200

    update term_wd script

--- bin/my_scripts/term_wd.sh
+++ bin/my_scripts/term_wd.sh
@@ -1,10 +1,19 @@
 #! /bin/sh
+
 WHEREAMI=$(cat /tmp/whereami)
 
-case $1 in
-	# "st") /bin/sh -c 'cd "$WHEREAMI" ; "st"' ;;
-	"st") st -d $WHEREAMI || st ;;
-	"urxvt") urxvt -cd "$WHEREAMI" || urxvt ;;
-	"alacritty") alacritty --working-directory "$WHEREAMI" || alacritty ;;
-esac
+# Check if the directory exists
+if [ -d "$WHEREAMI" ]; then
+    case $1 in
+        "st") st -d "$WHEREAMI" || st ;;
+        "urxvt") urxvt -cd "$WHEREAMI" || urxvt ;;
+        "alacritty") alacritty --working-directory "$WHEREAMI" || alacritty ;;
+    esac
+else
+    case $1 in
+        "st") st ;;
+        "urxvt") urxvt ;;
+        "alacritty") alacritty ;;
+    esac
+fi
 
