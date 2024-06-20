c26f6b976475bed2e72b249a32b48949661f1427
ornfelt <jonasornfelt@outlook.com>
2024-06-20 06:33:11 +0200

    sync dots script...

---
+++ sync_dots.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+TARGET_DIR="../dots"
+
+# Remove all files and subdirectories except for ".git" in the target directory
+find "$TARGET_DIR" -mindepth 1 -maxdepth 1 ! -name ".git" -exec rm -rf {} +
+
+# Copy all files and subdirectories except for ".git" from the current directory to the target directory
+find . -mindepth 1 -maxdepth 1 ! -name ".git" -exec cp -r {} "$TARGET_DIR" \;
+
+cd "$TARGET_DIR" || { echo "Failed to change directory to $TARGET_DIR"; exit 1; }
+git status
+
