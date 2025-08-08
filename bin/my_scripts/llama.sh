bac78413ed607fdee1ee4be23e6cc3c6b4937e9b
Jonas Ornfelt <jonasornfelt@outlook.com>
2025-08-08 23:29:50 +0200

    update local ai script

--- bin/my_scripts/llama.sh
+++ bin/my_scripts/llama.sh
@@ -7,7 +7,14 @@ if [[ -z "$code_root_dir" ]]; then
 fi
 
 # Define the model path
-MODEL_PATH="/mnt/new/2024/llama/Meta-Llama-3.1-8B-Instruct/Meta-Llama-3.1-8B-Instruct-ggml-model-Q4_K_M.gguf"
+#MODEL_PATH="/mnt/new/2024/llama/Meta-Llama-3.1-8B-Instruct/Meta-Llama-3.1-8B-Instruct-ggml-model-Q4_K_M.gguf"
+# TODO: first look in some local dir (in Documents somewhere)
+# new paths 
+#MODEL_PATH="/media/my_files/my_docs/ai/models/Meta-Llama-3.1-8B-Instruct-ggml-model-Q4_K_M.gguf"
+MODEL_PATH="/media/my_files/my_docs/ai/models/bartowski_Llama-3.2-3B-Instruct-GGUF_Llama-3.2-3B-Instruct-Q4_K_M.gguf"
+#MODEL_PATH="/media/my_files/my_docs/ai/models/ggml-org_gemma-3-1b-it-GGUF_gemma-3-1b-it-Q4_K_M.gguf"
+#MODEL_PATH="/media/my_files/my_docs/ai/models/unsloth_DeepSeek-R1-0528-Qwen3-8B-GGUF_DeepSeek-R1-0528-Qwen3-8B-Q4_K_M.gguf"
+#MODEL_PATH="/media/my_files/my_docs/ai/models/unsloth_DeepSeek-R1-0528-Qwen3-8B-GGUF_DeepSeek-R1-0528-Qwen3-8B-Q4_K_M.gguf.json"
 
 # Define the binary directory based on `code_root_dir`
 BINARY_DIR="${code_root_dir}/Code/ml/llama.cpp/build/bin"
