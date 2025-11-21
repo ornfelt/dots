e8aa479b1351d2bc48cad68d61761cd3130e3cc7
ornfelt <jonasornfelt@outlook.com>
2025-11-21 07:59:26 +0100

    update arch packages, yazi plugins and add new cava shaders

--- .config/yazi/plugins/fg.yazi/main.lua
+++ .config/yazi/plugins/fg.yazi/main.lua
@@ -102,7 +102,9 @@ local function setup(self, opts)
 end
 
 local function entry(_, job)
-	local _permit = ya.hide()
+	-- TODO: remove fallback after next stable release
+	local _permit = ui.hide and ui.hide() or ya.hide()
+
 	local fzf_version, err = Command("fzf"):arg("--version"):output()
 	if err then
 		return fail("`fzf` was not found")
@@ -141,7 +143,7 @@ local function entry(_, job)
 		local colon_pos = string.find(target, ":")
 		local file_url = colon_pos and string.sub(target, 1, colon_pos - 1) or target
 
-		ya.manager_emit("reveal", { file_url })
+		ya.emit("reveal", { file_url })
 	end
 end
 
