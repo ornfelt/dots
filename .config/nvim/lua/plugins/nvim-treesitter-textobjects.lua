036c0010503d53805ebc371239affc503dd3dcc3
ornfelt <jonasornfelt@outlook.com>
2025-12-22 12:39:28 +0100

    nvim: add commented main branch for treesitter plugins

--- .config/nvim/lua/plugins/nvim-treesitter-textobjects.lua
+++ .config/nvim/lua/plugins/nvim-treesitter-textobjects.lua
@@ -1,5 +1,6 @@
 return {
   "nvim-treesitter/nvim-treesitter-textobjects",
+  --branch = "main",
   dependencies = { "nvim-treesitter/nvim-treesitter" },
   lazy = false,
   --event = "VeryLazy",
--- .config/nvim/lua/plugins/nvim-treesitter.lua
+++ .config/nvim/lua/plugins/nvim-treesitter.lua
@@ -1,5 +1,6 @@
 return {
   "nvim-treesitter/nvim-treesitter",
+  --branch = "main",
   lazy = false,
   build = ":TSUpdate",
   --event = "VeryLazy",
