b9039e65550eb1e604f4f22a3d2182d383180742
Jonas Ornfelt <jonasornfelt@outlook.com>
2026-01-16 05:54:18 +0100

    nvim: update lazy loading logic in init.lua and update deb pkgs

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -2,17 +2,34 @@ require("config.lazy")
 require("lazy_plugins")
 require('options')
 
--- lazy load these
-vim.api.nvim_create_autocmd("UIEnter", {
-  once = true,
-  callback = function()
-    vim.schedule(function()
-      --require('lsp_new')
-      --require('custom_statusline')
-      require('lsp')
-      require('keybindings')
-      require('autocmds')
-    end)
-  end,
-})
+-- Note: lazy loading below technically works but is 
+-- a bit annoying since some autocmds aren't 
+-- applied to the opened file unless reloaded...
+local LAZY_LOAD = false
+
+-- eager loading
+if not LAZY_LOAD then
+  --require('lsp_new')
+  --require('custom_statusline')
+  require('lsp')
+  require('keybindings')
+  require('autocmds')
+end
+
+-- Lazy loading on UIEnter
+if LAZY_LOAD then
+  vim.api.nvim_create_autocmd("UIEnter", {
+    once = true,
+    callback = function()
+      vim.schedule(function()
+        -- lazy load these
+        --require('lsp_new')
+        --require('custom_statusline')
+        require('lsp')
+        require('keybindings')
+        require('autocmds')
+      end)
+    end,
+  })
+end
 
